677dca3f63f7043dc6fc6b16b06007cca6e45121
유환주 <yu1085@naver.com>
2023-05-28 21:42:08 +0900

    Webview Deploy Test

--- frontend/webview-client/src/Map.js
+++ frontend/webview-client/src/Map.js
@@ -10,6 +10,7 @@ function Map() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>webview-client</p>
         <p>어린이/장애인 안심귀가 모니터링 및 SOS 서비스</p>
+        <p>" ":data "</p>
         <p>{data ? data : "data undefined"}</p>
       </header>
     </div>
