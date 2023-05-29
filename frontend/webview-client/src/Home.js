56603a86c479346b6cbbca82f980d7affdcffb56
유환주 <yu1085@naver.com>
2023-05-29 19:05:28 +0900

    Webview Test

--- frontend/webview-client/src/Home.js
+++ frontend/webview-client/src/Home.js
@@ -7,6 +7,8 @@ function Home() {
     <div>
       <h1>어린이/장애인 안심귀가 모니터링 및 SOS 서비스</h1>
       <h3>HOME</h3>
+      <h3>HOME</h3>
+      <h3>HOME</h3>
       <button onClick={() => navigate("/map")}>GO MAP</button>
     </div>
   );
