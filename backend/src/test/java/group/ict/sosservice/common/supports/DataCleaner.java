3db4044916360dc58fe1b0e6dcf73833a1893646
LEE-sh1673 <lsh901673@gmail.com>
2023-08-28 10:13:17 +0900

    refactor: DataCleaner 로직 변경
    - 사용하지 않는 상수(TRUNCATE_FORMAT) 삭제
    - query 수행 시 DataSource 대신 JdbcTemplate 사용

--- backend/src/test/java/group/ict/sosservice/common/supports/DataCleaner.java
+++ backend/src/test/java/group/ict/sosservice/common/supports/DataCleaner.java
@@ -1,18 +1,14 @@
 package group.ict.sosservice.common.supports;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,16 +19,15 @@ public class DataCleaner implements InitializingBean {
     private static final String TRUNCATE_QUERY = "SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ' RESTART IDENTITY;') "
         + "AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = SCHEMA()";
 
-    private static final String TRUNCATE_FORMAT = "TRUNCATE TABLE %s";
-    public static final String REFERENTIAL_FORMAT = "SET REFERENTIAL_INTEGRITY %s";
+    private static final String REFERENTIAL_FORMAT = "SET REFERENTIAL_INTEGRITY %s";
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Autowired
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
-    private List<String> tableNames;
+    private List<String> truncateQueries;
 
     @Transactional
     public void clear() {
@@ -41,37 +36,13 @@ public class DataCleaner implements InitializingBean {
     }
 
     private void truncate() {
-        entityManager.createNativeQuery(String.format(REFERENTIAL_FORMAT, "FALSE")).executeUpdate();
-        for (String tableName : tableNames) {
-            entityManager.createNativeQuery(String.format(TRUNCATE_FORMAT, tableName)).executeUpdate();
-        }
-        entityManager.createNativeQuery(String.format(REFERENTIAL_FORMAT, "TRUE")).executeUpdate();
+        jdbcTemplate.execute(String.format(REFERENTIAL_FORMAT, "FALSE"));
+        truncateQueries.forEach(jdbcTemplate::execute);
+        jdbcTemplate.execute(String.format(REFERENTIAL_FORMAT, "TRUE"));
     }
 
     @Override
     public void afterPropertiesSet() {
-        try (final Connection connection = dataSource.getConnection()) {
-            mapTableNames(connection);
-        } catch (SQLException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    private void mapTableNames(final Connection connection)
-        throws SQLException {
-
-        final ResultSet tables =  truncateTables(connection);
-        tableNames = new ArrayList<>();
-
-        while (tables.next()) {
-            tableNames.add(tables.getString("TABLE_NAME"));
-        }
-    }
-
-    private static ResultSet truncateTables(final Connection connection)
-        throws SQLException {
-        return connection
-            .createStatement()
-            .executeQuery(TRUNCATE_QUERY);
+        truncateQueries = jdbcTemplate.queryForList(TRUNCATE_QUERY, String.class);
     }
 }
