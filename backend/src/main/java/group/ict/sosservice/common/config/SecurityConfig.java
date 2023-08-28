a7cc3fb6b0bd3de9d7f44760eb1be232b0079205
LEE-sh1673 <lsh901673@gmail.com>
2023-08-28 18:16:04 +0900

    feat: api 응답 클래스 추가
    * refactor: 사용되지 않는 import 문 정리
    
    - SecurityConfig

--- backend/src/main/java/group/ict/sosservice/common/config/SecurityConfig.java
+++ backend/src/main/java/group/ict/sosservice/common/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package group.ict.sosservice.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
